6ad43de5a5fae2251ce8e4b2a44a64b2d49cb71a
yusifhuseini <yusifhuseini@gmail.com>
2024-07-21 14:03:31 +0000

    Add SearchBox component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
-import Navbar from '@/components/Navbar';
 import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar />
+          <SearchBox />
           {children}
         </Providers>
       </body>
